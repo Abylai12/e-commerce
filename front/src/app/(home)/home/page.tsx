97250310a8dc9372d78face8785aa94b9cd4968c
Abylai12 <abilay1208@gmail.com>
2024-09-18 12:43:04 +0800

    header hiiv

--- front/src/app/(home)/home/page.tsx
+++ front/src/app/(home)/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Header from "@/components/header/header";
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-context-menu";
 import Image from "next/image";
@@ -13,14 +14,5 @@ export default function Home() {
   const plus = () => {
     setCount(count + 1);
   };
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
-      <h1>Welcome</h1>
-      <div className="flex">
-        <Button onClick={minus}>-</Button>
-        <Label>{count}</Label>
-        <Button onClick={plus}>+</Button>
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 }
---
+++ front/src/components/header/header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Input } from "../ui/input";
+import { Search } from "lucide-react";
+import { Heart } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <section className="flex bg-black px-6 py-4 justify-between">
+      <div className="flex items-center gap-8">
+        <div className="flex items-center gap-2">
+          <img src="./Vector.svg" alt="img" />
+          <span className="text-white text-sm">ECOMMERCE</span>
+        </div>
+        <span className="text-white">Ангилал</span>
+      </div>
+      <div className="py-2 px-4 rounded-[20px] bg-primary flex items-center gap-2">
+        <Search className="text-white text-2xl" />
+        <input
+          type="text"
+          className="bg-primary"
+          placeholder="Бүтээгдэхүүн хайх"
+        />
+      </div>
+      <div className="flex items-center gap-6">
+        <Heart className="text-white" />
+        <ShoppingCart className="text-white" />
+
+        <div>
+          <Button variant={"outline"}>Бүртгүүлэх</Button>
+          <Button variant={"myBtn"} className="ml-2">
+            Нэвтрэх
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
